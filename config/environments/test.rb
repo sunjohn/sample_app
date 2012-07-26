ba2f7ae9c912cc1a52ac68d077935ea69a7e7195
sunjohn <516689667@qq.com>
2012-07-26 17:23:50 +0800

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,7 +6,7 @@ SampleApp::Application.configure do
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
-
+  
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
@@ -34,4 +34,11 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  
+  # Speed up tests by lowering BCrypt's cost function.
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
+
 end
